95c3527d1e72595e0f64d5cd2367c82e136d9b71
Cedric van Putten <github@cedric.dev>
2024-03-16 03:48:38 +0100

    refactor: drop `expo-atlas/middleware` entry point

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import './utils/global';
 
 export type * from './data/types';
-
 export { MetroGraphSource } from './data/MetroGraphSource';
 export { StatsFileSource } from './data/StatsFileSource';
 
 export { AtlasError, AtlasValidationError } from './utils/errors';
-export { createStatsFile, validateStatsFile, getStatsMetdata, getStatsPath } from './utils/stats';
+export { createAtlasMiddleware } from './utils/middleware';
 export { fuzzyFilterModules } from './utils/search';
+export { createStatsFile, validateStatsFile, getStatsMetdata, getStatsPath } from './utils/stats';
