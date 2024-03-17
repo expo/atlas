1658f3af6e89fff65c6b58ce5ff2dd1ed67174cd
Cedric van Putten <github@cedric.dev>
2024-03-17 15:38:49 +0100

    chore: restore webui metro cache in ci (#10)
    * chore: restore webui metro cache in ci
    
    * chore: test webui metro cache

--- webui/metro.config.js
+++ webui/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { FileStore } = require('metro-cache');
 const { withNativeWind } = require('nativewind/metro');
 const path = require('path');
 
@@ -6,6 +7,12 @@ const config = withNativeWind(getDefaultConfig(__dirname), {
   input: './src/styles.css',
 });
 
+// Allow Metro to access the `~plugin/**` files
 config.watchFolders = [__dirname, path.resolve(__dirname, '..')];
+
+// Move the Metro cache to `node_modules/.cache`
+config.cacheStores = [
+  new FileStore({ root: path.join(__dirname, 'node_modules', '.cache', 'metro') }),
+];
 
 module.exports = config;
