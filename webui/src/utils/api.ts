9af692e1c03ad60370e791daee17b57488c6ac4a
Cedric van Putten <github@cedric.dev>
2024-03-27 16:08:59 +0100

    fix: local webui development

--- webui/src/utils/api.ts
+++ webui/src/utils/api.ts
@@ -1,9 +1,10 @@
 /**
  * Keep this path in sync with `app.json`'s `baseUrl`.
+ * The base url is disabled when running webui in development, as `baseUrl` is for export only.
  *
  * @see https://docs.expo.dev/versions/latest/config/app/#baseurl
  */
-const baseUrl = '/_expo/atlas';
+const baseUrl = __DEV__ ? '' : '/_expo/atlas';
 
 /**
  * Fetch data from the API routes, adding the `baseUrl` to all requests.
