1bb5fab841821a1dd0f5bc58615375ed164ac0a2
Cedric van Putten <github@cedric.dev>
2024-03-29 15:20:54 +0100

    fix: check if `--no-open` is true instead of false

--- src/cli/resolveOptions.ts
+++ src/cli/resolveOptions.ts
@@ -9,7 +9,7 @@ export type Options = Awaited<ReturnType<typeof resolveOptions>>;
 export async function resolveOptions(input: Input) {
   const statsFile = await resolveStatsFile(input);
   const port = await resolvePort(input);
-  return { statsFile, port, browserOpen: input['--no-open'] !== false };
+  return { statsFile, port, browserOpen: input['--no-open'] !== true };
 }
 
 async function resolveStatsFile(input: Input) {
