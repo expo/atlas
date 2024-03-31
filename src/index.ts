9bc3c4edfb209771b5d5cf8988e885279085dc25
Cedric van Putten <github@cedric.dev>
2024-03-31 23:28:18 +0200

    refactor: simplify filters by moving back to picomatch

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,4 @@ export { StatsFileSource } from './data/StatsFileSource';
 
 export { AtlasError, AtlasValidationError } from './utils/errors';
 export { createAtlasMiddleware } from './utils/middleware';
-export { fuzzyFilterModules } from './utils/search';
 export { createStatsFile, validateStatsFile, getStatsMetdata, getStatsPath } from './utils/stats';
