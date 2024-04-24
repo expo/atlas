e14148ffdbeb5020a06b13a58778ed1828336e49
Cedric van Putten <github@cedric.dev>
2024-04-24 15:58:31 +0200

    refactor(webui): style bundle selection form more like Expo

--- webui/src/components/BundleSelectForm.tsx
+++ webui/src/components/BundleSelectForm.tsx
@@ -1,10 +1,8 @@
 import * as Select from '@radix-ui/react-select';
-import cn from 'classnames';
+import { cx } from 'class-variance-authority';
 import { useRouter } from 'expo-router';
 // @ts-expect-error
 import ChevronDownIcon from 'lucide-react/dist/esm/icons/chevron-down';
-// @ts-expect-error
-import ChevronUpIcon from 'lucide-react/dist/esm/icons/chevron-up';
 
 import { useBundle } from '~/providers/bundle';
 import { Button } from '~/ui/Button';
@@ -30,20 +28,17 @@ export function BundleSelectForm() {
         <Select.Content
           side="bottom"
           collisionPadding={{ left: 16, right: 16 }}
-          className={cn(
+          className={cx(
             'flex min-w-[220px] flex-col gap-0.5 rounded-md border border-default bg-default p-1 shadow-md',
             'transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-200 data-[state=open]:duration-300',
             'data-[state=closed]:fade-out data-[state=closed]:slide-out-to-top-1/3 data-[state=open]:fade-in data-[state=open]:slide-in-from-top-1/3'
           )}
         >
-          <Select.ScrollUpButton className="SelectScrollButton">
-            <ChevronUpIcon />
-          </Select.ScrollUpButton>
-          <Select.Viewport className="SelectViewport">
+          <Select.Viewport className="py-2">
             {bundles.map((item) => (
               <div key={item.id}>
                 <Select.Item value={item.id} asChild>
-                  <Button variant="quaternary" size="sm" className="w-full">
+                  <Button variant="quaternary" size="sm" className="w-full !justify-start my-0.5">
                     <Tag variant={item.platform} size="xs" className="mr-2" />
                     <Select.ItemText>{relativeBundlePath(item, item.entryPoint)}</Select.ItemText>
                   </Button>
@@ -51,9 +46,6 @@ export function BundleSelectForm() {
               </div>
             ))}
           </Select.Viewport>
-          <Select.ScrollDownButton className="SelectScrollButton">
-            <ChevronDownIcon />
-          </Select.ScrollDownButton>
         </Select.Content>
       </Select.Portal>
     </Select.Root>
--- webui/src/ui/Button.tsx
+++ webui/src/ui/Button.tsx
@@ -6,7 +6,6 @@ import cn from 'classnames';
 import { type ButtonHTMLAttributes, forwardRef } from 'react';
 
 const buttonVariants = cva(
-  // 'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors outline-none disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
@@ -71,9 +70,7 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
-    return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
-    );
+    return <Comp className={buttonVariants({ variant, size, className })} ref={ref} {...props} />;
   }
 );
 Button.displayName = 'Button';
