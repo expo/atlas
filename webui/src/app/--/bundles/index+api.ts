bfca65684a7fdd7909dc5a9aa972cbabb5013e8c
Cedric van Putten <github@cedric.dev>
2024-05-18 17:46:46 +0200

    fix(webui): use relative entry file paths

--- webui/src/app/--/bundles/index+api.ts
+++ webui/src/app/--/bundles/index+api.ts
@@ -3,8 +3,18 @@ import type { PartialAtlasBundle } from '~core/data/types';
 
 export async function GET() {
   try {
-    const bundles = await getSource().listBundles();
-    return Response.json(bundles.sort(sortBundlesByPlatform));
+    const bundles = (await getSource().listBundles()).sort(sortBundlesByPlatform);
+    const bundlesWithRelativeEntry = bundles.map((bundle) => ({
+      ...bundle,
+      // TODO(cedric): this is a temporary workaround to make entry points look better on Windows
+      // Need to figure out something better, without breaking the streaming-lookup of exported Atlas files.
+      entryPoint: bundle.entryPoint
+        .replace(bundle.sharedRoot, '') // Make the entry point relative to the shared root
+        .replace(/\\+/g, '/') // Normalize Windows paths to POSIX paths
+        .replace(/^\//, ''), // Remove any leading slashes
+    }));
+
+    return Response.json(bundlesWithRelativeEntry);
   } catch (error: any) {
     return Response.json({ error: error.message }, { status: 406 });
   }
