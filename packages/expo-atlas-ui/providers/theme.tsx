63736d61dfe803fafe1e1ff72af7468b77ef9abc
Cedric van Putten <github@cedric.dev>
2024-09-26 13:51:31 +0200

    chore: add expo dark from expo style guide

--- packages/expo-atlas-ui/providers/theme.tsx
+++ packages/expo-atlas-ui/providers/theme.tsx
@@ -1,16 +1,21 @@
-import { useColorScheme } from 'nativewind';
-import { useEffect, type PropsWithChildren } from 'react';
+import '~/styles/global.css';
+import '~/styles/expo-dark.css';
+
+// Import the Expo-required radix styles
+import '@radix-ui/colors/greenDark.css';
+import '@radix-ui/colors/yellowDark.css';
+import '@radix-ui/colors/redDark.css';
+import '@radix-ui/colors/blueDark.css';
+import '@radix-ui/colors/orangeDark.css';
+import '@radix-ui/colors/purpleDark.css';
+import '@radix-ui/colors/pinkDark.css';
+import '@radix-ui/colors/slateDark.css';
+// NOTE(cedric): these are not imported by `@expo/styleguide/dist/expo-theme.css`, but they are required for the syntax highlighting
+import '@radix-ui/colors/grayDark.css';
+
+import type { PropsWithChildren } from 'react';
 
 export function ThemeProvider({ children }: PropsWithChildren) {
-  const { colorScheme } = useColorScheme();
-
-  useEffect(() => {
-    // Keep the prefered color scheme synced with the `body` class name
-    if (document.body && colorScheme) {
-      document.body.classList.remove('light-theme', 'dark-theme');
-      document.body.className = `${colorScheme}-theme`;
-    }
-  }, [colorScheme]);
-
+  // This provider only imports styling, doesn't need to interact with the React tree
   return children;
 }
