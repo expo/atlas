4237df5245438f2d13ec53625c6e06e2deede368
Cedric van Putten <github@cedric.dev>
2025-08-16 13:48:09 +0200

    fix(atlas-ui): avoid an issue with NativeWind in development not generating used styles

--- packages/expo-atlas-ui/tailwind.config.js
+++ packages/expo-atlas-ui/tailwind.config.js
@@ -4,7 +4,13 @@ const expoTheme = require('@expo/styleguide/tailwind');
 module.exports = {
   ...expoTheme,
   darkMode: ['class', 'dark-theme'],
-  content: ['./{app,components,providers,ui,utils}**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './providers/**/*.{js,jsx,ts,tsx}',
+    './ui/**/*.{js,jsx,ts,tsx}',
+    './utils/**/*.{js,jsx,ts,tsx}',
+  ],
   presets: [require('nativewind/preset')],
   plugins: [require('tailwindcss-animate'), require('tailwind-gradient-mask-image')],
 };
