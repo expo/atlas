cb7bdd609d271934839aa80cc2ce767bec114643
Cedric van Putten <github@cedric.dev>
2025-08-16 12:00:46 +0200

    fix(atlas-ui): move Radix CSS imports to `global.css` to avoid node external issue

--- packages/expo-atlas-ui/providers/theme.tsx
+++ packages/expo-atlas-ui/providers/theme.tsx
@@ -1,17 +1,6 @@
 import '~/styles/global.css';
 import '~/styles/expo-dark.css';
 
-// Import the Expo-required radix styles from `@expo/style-guide/dist/expo-theme.css`
-import '@radix-ui/colors/greenDark.css';
-import '@radix-ui/colors/yellowDark.css';
-import '@radix-ui/colors/redDark.css';
-import '@radix-ui/colors/blueDark.css';
-import '@radix-ui/colors/orangeDark.css';
-import '@radix-ui/colors/purpleDark.css';
-import '@radix-ui/colors/pinkDark.css';
-import '@radix-ui/colors/slateDark.css';
-import '@radix-ui/colors/grayDark.css'; // NOTE(cedric): this is required for syntax highlighting
-
 import { useColorScheme } from 'nativewind';
 import type { PropsWithChildren } from 'react';
 
